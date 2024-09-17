8810dc07c33c97834f3e050eed2ac6f887d57854
MoustafaMohamedFaried <moustafa17mohamed@gmail.com>
2024-09-17 12:12:56 +0300

    update sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,96 +99,42 @@
 
         @auth
             {{-- sidebar & content --}}
-            <nav class="nav d-flex row">
+            <div class="d-flex">
+                <div class="sidebar-navigation col-md-2">
+                    <ul>
+                        <li><a href="#">Users Management <em class="mdi mdi-chevron-down"></em></a>
+                            <ul>
+                                <li><a href="{{ route('users.index') }}">Users</a></li>
+                                <li><a href="{{ route('users.registerRequests') }}">Register Requestes</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">College Management <em class="mdi mdi-chevron-down"></em></a>
+                            <ul>
+                                <li><a href="{{ route('headquarters.index') }}">Headquarters</a></li>
+                                <li><a href="{{ route('faculties.index') }}">Faculties</a></li>
+                                <li><a href="{{ route('departments.index') }}">Departments</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Topics Management <em class="mdi mdi-chevron-down"></em></a>
+                            <ul>
+                                <li><a href="{{ route('topics.index') }}">Topics</a></li>
+                                <li><a href="{{ route('agendas.index') }}">Agendas</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Session Management <em class="mdi mdi-chevron-down"></em></a>
+                            <ul>
+                                <li><a href="{{ route('sessions-departments.index') }}">Department</a></li>
+                                <li><a href="#">College Council</a></li>
+                                <li><a href="#">Faculty</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
 
-                <!-- Sidebar content -->
-                <ul class="nav flex-column col-md-2 mt-2" style="display: inline">
-
-                    <!-- Dropdown Menu Users Management -->
-                    <li class="nav-item dropdown">
-                        <button class="btn btn-lg btn-secondary m-2 w-100" type="button"
-                            onclick="toggleVisibility('usersManagement')">
-                            Users Management
-                        </button>
-
-                        <div class="dropdown-menu text-body-secondary text-center" id="usersManagement"
-                            style="display: none; width: 16rem;">
-
-                            <a type="button" href="{{ route('users.index') }}" class="btn btn-light w-100">Users</a>
-                            <a type="button" href="{{ route('users.registerRequests') }}"
-                                class="btn btn-light w-100">Register Requests</a>
-                        </div>
-                    </li>
-
-                    <!-- Dropdown Menu College Management -->
-                    <li class="nav-item dropdown">
-                        <button class="btn btn-lg btn-secondary m-2 w-100" type="button"
-                            onclick="toggleVisibility('collegeManagement')">
-                            College Management
-                        </button>
-
-                        <div class="dropdown-menu text-body-secondary text-center" id="collegeManagement"
-                            style="display: none; width: 16rem;">
-                            <a type="button" href="{{ route('headquarters.index') }}"
-                                class="btn btn-light w-100">Headquarters</a>
-                            <a type="button" href="{{ route('faculties.index') }}"
-                                class="btn btn-light w-100">Faculties</a>
-                            <a type="button" href="{{ route('departments.index') }}"
-                                class="btn btn-light w-100">Departments</a>
-                        </div>
-
-                    </li>
-
-                    <!-- Dropdown Menu Topics Management -->
-                    <li class="nav-item dropdown">
-
-                        <button class="btn btn-lg btn-secondary m-2 w-100" type="button"
-                            onclick="toggleVisibility('topicsManagement')">
-                            Topics Management
-                        </button>
-
-                        <div class="dropdown-menu text-body-secondary text-center" id="topicsManagement"
-                            style="display: none; width: 16rem;">
-
-                            <a type="button" href="{{ route('topics.index') }}" class="btn btn-light w-100">Topics</a>
-                            <a type="button" href="{{ route('agendas.index') }}"
-                                class="btn btn-light w-100">Agendas</a>
-
-                        </div>
-
-                    </li>
-
-                    <!-- Dropdown Menu Sessions Management -->
-                    <li class="nav-item dropdown">
-
-                        <button class="btn btn-lg btn-secondary m-2 w-100" type="button"
-                            onclick="toggleVisibility('sessionsManagement')">
-                            Sessions Management
-                        </button>
-
-                        <div class="dropdown-menu text-body-secondary text-center" id="sessionsManagement"
-                            style="display: none; width: 16rem;">
-
-                            <a type="button" href="{{ route('sessions-departments.index') }}"
-                                class="btn btn-light w-100">Department
-                                Sessions</a>
-                            <a type="button" href="#" class="btn btn-light w-100">College
-                                Council</a>
-                            <a type="button" href="{{ route('sessions-faculties.index') }}"
-                                class="btn btn-light w-100">Faculty
-                                Sessions</a>
-
-                        </div>
-
-                    </li>
-
-                </ul>
-
-                <!-- Main content -->
                 <main class="p-5 col-md-10">
                     @yield('content')
                 </main>
-            </nav>
+            </div>
         @endauth
 
         <!-- Main content if guest-->
@@ -200,25 +146,209 @@
 
     </div>
 
-    <script>
-        function toggleVisibility(sectionId) {
-            // Select the target div
-            var section = document.getElementById(sectionId);
-
-            // Toggle display property
-            if (section.style.display === 'none' || section.style.display === '') {
-                // Hide all sections first
-                document.querySelectorAll('.dropdown-menu').forEach(function(div) {
-                    div.style.display = 'none';
-                });
-
-                // Then show the clicked section
-                section.style.display = 'block';
-            } else {
-                // Hide the section if it's already visible
-                section.style.display = 'none';
-            }
+    <style>
+        .sidebar-navigation {
+            width: 200px;
+            height: auto;
+            background-color: #fff;
+            margin: 20px auto;
+            webkit-box-shadow: 3px 5px 10px 0px rgba(0, 0, 0, 0.16);
+            -moz-box-shadow: 3px 5px 10px 0px rgba(0, 0, 0, 0.16);
+            box-shadow: 3px 5px 10px 0px rgba(0, 0, 0, 0.16);
+            font-family: "Poppins", sans-serif;
+            font-size: 12px;
         }
+
+        .sidebar-navigation .title {
+            display: block;
+            font-size: 1.2em;
+            background-color: #1e1e1e;
+            padding: 20px 25px;
+            color: #fff;
+            font-weight: 600;
+            text-transform: uppercase;
+            letter-spacing: 0.1em;
+        }
+
+        .sidebar-navigation>ul>li>a {
+            text-transform: uppercase;
+        }
+
+        .sidebar-navigation ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        .sidebar-navigation ul li {
+            display: block;
+        }
+
+        .sidebar-navigation ul li a {
+            position: relative;
+            display: block;
+            font-size: 1em;
+            font-weight: 600;
+            padding: 20px 25px;
+            text-decoration: none;
+            color: #2e2e2e;
+            letter-spacing: 0.02em;
+            /* border-bottom: 1px solid #eee; */
+            -webkit-transition: all 0.3s linear;
+            -moz-transition: all 0.3s linear;
+            -o-transition: all 0.3s linear;
+            transition: all 0.3s linear;
+        }
+
+        .sidebar-navigation ul li a em {
+            font-size: 24px;
+            position: absolute;
+            right: 20px;
+            top: 50%;
+            transform: translateY(-50%);
+            padding: 5px;
+            border-radius: 50%;
+        }
+
+        .sidebar-navigation ul li:hover>a,
+        .sidebar-navigation ul li.selected>a {
+            background-color: #ecf0f1;
+            color: #495d62;
+            border-color: rgba(255, 255, 255, 0.1);
+        }
+
+        .sidebar-navigation ul li ul {
+            display: none;
+        }
+
+        .sidebar-navigation ul li ul.open {
+            display: block;
+        }
+
+        .sidebar-navigation ul li ul li a {
+            color: #495d62;
+            border-color: rgba(255, 255, 255, 0.1);
+        }
+
+        .sidebar-navigation ul li ul li a:before {
+            content: "";
+            width: 10px;
+            height: 1px;
+            margin-right: 5px;
+            display: inline-block;
+            vertical-align: middle;
+            background-color: #495d62;
+            -webkit-transition: all 0.2s linear;
+            -moz-transition: all 0.2s linear;
+            -o-transition: all 0.2s linear;
+            transition: all 0.2s linear;
+        }
+
+        .sidebar-navigation ul li ul li:hover>a,
+        .sidebar-navigation ul li ul li.selected>a {
+            background-color: #e6ebed;
+        }
+
+        .sidebar-navigation ul li ul li:hover>a:before,
+        .sidebar-navigation ul li ul li.selected>a:before {
+            margin-right: 10px;
+        }
+
+        .sidebar-navigation ul li ul li.selected.selected--last>a {
+            background-color: #94aab0;
+            color: #fff;
+        }
+
+        .sidebar-navigation ul li ul li.selected.selected--last>a:before {
+            background-color: #fff;
+        }
+
+        .subMenuColor1 {
+            background-color: #fbfcfc;
+        }
+
+        .subMenuColor2 {
+            background-color: white;
+        }
+    </style>
+
+    <script>
+        $(function() {
+            var $ul = $('.sidebar-navigation > ul');
+
+            $ul.find('li a').click(function(e) {
+                var $li = $(this).parent();
+
+                if ($li.find('ul').length > 0) {
+                    e.preventDefault();
+
+                    if ($li.hasClass('selected')) {
+                        $li.removeClass('selected').find('li').removeClass('selected');
+                        $li.find('ul').slideUp(400);
+                        $li.find('a em').removeClass('mdi-flip-v');
+                    } else {
+
+                        if ($li.parents('li.selected').length == 0) {
+                            $ul.find('li').removeClass('selected');
+                            $ul.find('ul').slideUp(400);
+                            $ul.find('li a em').removeClass('mdi-flip-v');
+                        } else {
+                            $li.parent().find('li').removeClass('selected');
+                            $li.parent().find('> li ul').slideUp(400);
+                            $li.parent().find('> li a em').removeClass('mdi-flip-v');
+                        }
+
+                        $li.addClass('selected');
+                        $li.find('>ul').slideDown(400);
+                        $li.find('>a>em').addClass('mdi-flip-v');
+                    }
+                }
+            });
+
+
+            $('.sidebar-navigation > ul ul').each(function(i) {
+                if ($(this).find('>li>ul').length > 0) {
+                    var paddingLeft = $(this).parent().parent().find('>li>a').css('padding-left');
+                    var pIntPLeft = parseInt(paddingLeft);
+                    var result = pIntPLeft + 20;
+
+                    $(this).find('>li>a').css('padding-left', result);
+                } else {
+                    var paddingLeft = $(this).parent().parent().find('>li>a').css('padding-left');
+                    var pIntPLeft = parseInt(paddingLeft);
+                    var result = pIntPLeft + 20;
+
+                    $(this).find('>li>a').css('padding-left', result).parent().addClass('selected--last');
+                }
+            });
+
+            var t = ' li > ul ';
+            for (var i = 1; i <= 10; i++) {
+                $('.sidebar-navigation > ul > ' + t.repeat(i)).addClass('subMenuColor' + i);
+            }
+
+            var activeLi = $('li.selected');
+            if (activeLi.length) {
+                opener(activeLi);
+            }
+
+            function opener(li) {
+                var ul = li.closest('ul');
+                if (ul.length) {
+
+                    li.addClass('selected');
+                    ul.addClass('open');
+                    li.find('>a>em').addClass('mdi-flip-v');
+
+                    if (ul.closest('li').length) {
+                        opener(ul.closest('li'));
+                    } else {
+                        return false;
+                    }
+
+                }
+            }
+
+        });
     </script>
 </body>
 
